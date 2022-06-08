bd496e20cf1359a11637200a48ef92eb5976e124
Lazaro03 <95556479+Lazaro03@users.noreply.github.com>
2022-06-08 16:43:09 -0400

    Update main.py

--- main.py
+++ main.py
@@ -651,7 +651,7 @@ def onmessage(update,bot:ObigramClient):
             start_msg+= '🙂Si usted desea añadir la barra de comandos al menú de acceso rápido de su bot envíe /commands.\n\n'
             start_msg+= '😁𝚀𝚞𝚎 𝚍𝚒𝚜𝚏𝚛𝚞𝚝𝚎 𝚐𝚛𝚊𝚗𝚍𝚎𝚖𝚎𝚗𝚝𝚎 𝚜𝚞 𝚎𝚜𝚝𝚊𝚍𝚒𝚊😁.\n'
             bot.editMessageText(message,start_msg)
-            message = bot.sendMessage(update.message.chat.id,'🦾)'
+            message = bot.sendMessage(update.message.chat.id,'🦾')
         elif '/files' == msgText and user_info['cloudtype']=='moodle':
              proxy = ProxyCloud.parse(user_info['proxy'])
              client = MoodleClient(user_info['moodle_user'],
