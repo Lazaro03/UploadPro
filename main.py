be945a189b051ae25ff18d2e202631ebe3901f9a
Lazaro03 <95556479+Lazaro03@users.noreply.github.com>
2022-06-08 16:31:52 -0400

    Update main.py

--- main.py
+++ main.py
@@ -643,14 +643,17 @@ def onmessage(update,bot:ObigramClient):
             start_msg = '   🌟𝔹𝕆𝕋 𝕀ℕ𝕀ℂ𝕀𝔸𝔻𝕆🌟\n'
             start_msg+= '࿇ ══━━━━✥◈✥━━━━══ ࿇\n'
             start_msg+= '🤖Hola @' + str(username)+'\n'
-            start_msg+= '☺️! Bienvenid@ al bot de descargas gratis SuperDownload en su versión inicial 1.0 PlusEdition🌟!\n'
+            start_msg+= '☺️! Bienvenid@ al bot de descargas gratis Up-Posgrado en su versión inicial 1.0 PlusEdition🌟!\n'
             start_msg+= '🙂Si necesita ayuda o información utilice:\n'
             start_msg+= '/help\n'
             start_msg+= '/about\n'
+            start_msg+= '👤O escríbale a mi Admin @Michel1203👤
             start_msg+= '🙂Si usted desea añadir la barra de comandos al menú de acceso rápido de su bot envíe /commands.\n\n'
             start_msg+= '😁𝚀𝚞𝚎 𝚍𝚒𝚜𝚏𝚛𝚞𝚝𝚎 𝚐𝚛𝚊𝚗𝚍𝚎𝚖𝚎𝚗𝚝𝚎 𝚜𝚞 𝚎𝚜𝚝𝚊𝚍𝚒𝚊😁.\n'
             bot.editMessageText(message,start_msg)
             message = bot.sendMessage(update.message.chat.id,'🦾')
+          else:
+               bot.sendMessage(update.message.chat.id,'⚠️No posee permisos de administrador⚠️')
         elif '/files' == msgText and user_info['cloudtype']=='moodle':
              proxy = ProxyCloud.parse(user_info['proxy'])
              client = MoodleClient(user_info['moodle_user'],
